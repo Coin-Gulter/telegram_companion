043a66f19766b1c08d90e1a33f6f699287b3e377
Coin-Gulter <duvalder230@gmail.com>
2023-08-15 16:06:53 +0300

    Added comments.

--- main.py
+++ main.py
@@ -5,19 +5,28 @@ import os
 import sys
 import time
 import tiktoken
+
 import langdetect
-import google
-from telegram import Update, Chat, ReplyKeyboardMarkup, KeyboardButton
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, filters
-from google.cloud import texttospeech
 from langdetect import detect
 
+import google
+from google.cloud import texttospeech
+
+from telegram import Update
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, filters
+
+# Set the path to the project directory
 PATH = os.getcwd()
+
+# Define the paths to the chat history and keys access directories
 PATH_CHAT_HISTORY = os.path.join(PATH, 'chat_history')
 PATH_KEYS_ACCESS = os.path.join(PATH, 'keys_access')
 
+# Define the name of the AI model to use and telegram bot username
 BOT_USERNAME = 'soundspeakerbot'
 AI_MODEL_NAME = "gpt-3.5-turbo"
+
+# Define the system message that will be sent to the user when the bot starts
 SYSTEM_MESSAGE = f"""
                     Your name is "Speaky".
                     Your mission is to speak effectively and fast.
@@ -34,28 +43,46 @@ SYSTEM_MESSAGE = f"""
                     Also you must use the same languge in your answear as the user in his prompt.
                 """
 
+# Define the maximum number of messages that can be stored in the chat history file
 MAX_CHAT_HISTORY_LEN = 10000
+
+# Define the maximum number of last messages that used in gpt dialog messages
 MAX_CHAT_MEMORY_LEN = 40
+
+# Define the maximum number of tokens that can be used by the AI model at all
 MAX_TOKENS = 500
+
+# Define the maximum number of tokens that can be used for the output of the AI model
 MAX_OUTPUT_TOKENS = 60
 
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+
+# Define a function to create a comment for the user prompt
 make_user_prompt = lambda text: {"role": "user", "content": f"""
                                                             {text}
 
                                                             <Be brief and responde in 1-15 words.>                        
                                                             """}
 
+# Define a function to get a number messages from the chat history
 get_message_from_history = lambda chat_history, number: chat_history[list(chat_history.keys())[number]] 
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
+# Define a function to check if a path exists
 def check_if_needed_path_exist(pathes=[PATH_CHAT_HISTORY,PATH_KEYS_ACCESS]):
+    """Check if the needed paths exist.
+    Args:
+        pathes (list): The list of paths to check.
+    Returns:
+        None
+    """
     for path in pathes:
         full_path = os.path.join(PATH, path)
         if not os.path.exists(full_path):
             os.mkdir(full_path)
             print(f'Folder - "{full_path}" created')
 
+# Define a function to get the number of tokens used by a list of messages.
 def num_tokens_from_messages(messages, model="gpt-3.5-turbo-0613"):
     """Return the number of tokens used by a list of messages."""
     try:
@@ -94,8 +121,15 @@ def num_tokens_from_messages(messages, model="gpt-3.5-turbo-0613"):
     num_tokens += 3 
     return num_tokens
 
+# Define a function to load a json file as a dict
 def load_json_file(file_name, path=PATH_CHAT_HISTORY):
-    """Loads a JSON file as a dictionary."""
+    """Loads a JSON file as a dictionary.
+    Args:
+        file_name (str): The name of the file without format.
+        path (str): The path to the directory where the file is located.
+    Returns:
+        dict: The contents of the file as a dictionary.
+    """
     file = os.path.join(PATH, path, str(file_name +'.json'))
     if os.path.isfile(file):
         try:
@@ -108,7 +142,22 @@ def load_json_file(file_name, path=PATH_CHAT_HISTORY):
     
     return False
 
+# Define a function to save messages in correct format into file
 def save_message(chat_name, chat_id, user_id, message_id, username, message_text, chat_path=PATH_CHAT_HISTORY):
+    """Saves a message to the chat history.
+    Args:
+        chat_name (str): The name of the chat.
+        chat_id (int): The ID of the chat.
+        user_id (int): The ID of the user.
+        message_id (int): The ID of the message.
+        username (str): The username of the user.
+        message_text (str): The text of the message.
+        chat_path (str): The path to the directory where the chat history is stored.
+    Returns:
+        The updated chat history.
+    """
+
+    # Get the message data.
     message_data = { message_id:{
         "chat_id": chat_id,
         "user_id": user_id,
@@ -117,43 +166,68 @@ def save_message(chat_name, chat_id, user_id, message_id, username, message_text
     }
     }
 
+    # Load the chat history.
     chat_history = load_json_file(chat_name)
 
+    # If the chat history exists, update it.
     if chat_history:
         if len(chat_history) >= MAX_CHAT_HISTORY_LEN:
+            # If number of messages too many remove the oldest message from the chat history.
             del chat_history[list(chat_history.keys())[0]]
 
+        # Update the chat history with the new message.
         chat_history.update(message_data)
+
+    # Otherwise, create a new chat history.
     else:
         chat_history = message_data
 
+    # Save the chat history to file.
     with open(os.path.join(chat_path, chat_name +'.json'), "w") as file:
         json.dump(chat_history, file)
 
+    # Return the updated chat history.
     chat_history = load_json_file(chat_name)
     return chat_history
 
+# Define a function to make a messages dialog for a gpt model to get completion
 def make_chatbot_history(chat_history):
+    """Creates a chatbot history from the chat history.
+    Args:
+        chat_history (dict): The chat history in dict format from "load_json_file".
+    Returns:
+        The chatbot history.
+    """
+
+    # Initialize the chatbot history.
     all_messages =  [{'role':'system', 'content':SYSTEM_MESSAGE}]
     messages = []
 
     chat_key_list = list(chat_history.keys())
 
+    # Iterate over the chat history.
     for index, key in enumerate(chat_key_list):
-        if chat_history[key]["username"] == BOT_USERNAME:
-            messages.append({'role':'assistant', 'content': chat_history[key]["message_text"]})
-        elif chat_history[key]["username"]:
+        # Get the message data.
+        massege_data = chat_history[key]
+
+        # Add the message to the chatbot history.
+        if massege_data["username"] == BOT_USERNAME:
+            messages.append({'role':'assistant', 'content': massege_data["message_text"]})
+        else:
             if (index+1) == len(chat_key_list):
-                messages.append(make_user_prompt(chat_history[key]["message_text"]))
+                messages.append(make_user_prompt(massege_data["message_text"]))
             else:
-                messages.append({'role':'user', 'content': chat_history[key]["message_text"]})
+                messages.append({'role':'user', 'content': massege_data["message_text"]})
     
+    # If the chatbot history is too long, remove the oldest messages.
     if len(messages) > MAX_CHAT_MEMORY_LEN:
         messages = messages[-MAX_CHAT_MEMORY_LEN:]
 
+    # Count the number of tokens in the chatbot history.
     tokens_number = num_tokens_from_messages(messages, model=AI_MODEL_NAME)
     print('Start tokens', tokens_number)
 
+    # Remove messages from the chatbot history until the number of tokens is less than or equal to the maximum number of tokens.
     while tokens_number > MAX_TOKENS:
         del messages[0]
         tokens_number = num_tokens_from_messages(messages, model=AI_MODEL_NAME)
@@ -164,9 +238,20 @@ def make_chatbot_history(chat_history):
     print('End tokens', tokens_number)
     print('_________________________________________________________',all_messages)
         
+    # Return the chatbot history.
     return all_messages
 
+# Define a function to get a completion of gpt model
 def get_completion(messages, model=AI_MODEL_NAME, max_out_tokens=MAX_OUTPUT_TOKENS):
+    """Gets a completion from the AI model.
+    Args:
+        messages (list): The list of messages from "make_chatbot_history" function.
+        model (str): The name of the AI model.
+        max_out_tokens (int): The maximum number of tokens in the completion.
+    Returns:
+        The completion.
+    """
+    # Try to get a completion from the AI model.
     try:
         print('try create completion')
         response = openai.ChatCompletion.create(
@@ -178,37 +263,54 @@ def get_completion(messages, model=AI_MODEL_NAME, max_out_tokens=MAX_OUTPUT_TOKE
         )
         response = response.choices[0].message["content"]
     except openai.error.APIError:
+        # If an error occurs, return a default message.
         print('except APIError create completion')
         response = "Sorry, something went wrong. 😒\n I can't answear your question. 😅"
 
     return response
 
-
+# define a function to react on /start command message.
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    print(update)
-    match update.message.chat.type:
-        case Chat.PRIVATE:
-            username = update.effective_user.username
-            user_id = update.effective_user.id
-            message_id = update.message.message_id
+    """The `/start` handler.
 
-            save_message(username, user_id,  user_id, message_id, username, message_text='/start')
+    Args:
+        update (Update): The update object.
+        context (ContextTypes.DEFAULT_TYPE): The context object.
+    """
+     
+    # Get the user information.
+    username = update.effective_user.username
+    user_id = update.effective_user.id
+    message_id = update.message.message_id
 
-            await context.bot.send_message(chat_id=update.effective_chat.id, text="""Hello, I'm your personal companion. 😄\n
-                                                                                    You can talk with me and ask me an intresting question.""")
-            
-            save_message(username, user_id,  user_id, message_id+1, BOT_USERNAME, message_text= """Hello, I'm your personal companion. 😄\n
-                                                                                    You can talk with me and ask me an intresting question.""")
+    # Save the message to the chat history.
+    save_message(username, user_id,  user_id, message_id, username, message_text='/start')
+
+    # Send a welcome message to the user.
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="""Hello, I'm your personal companion. 😄\n
+                                                                            You can talk with me and ask me an intresting question.""")
+    # Save the bot's message to the chat history.
+    save_message(username, user_id,  user_id, message_id+1, BOT_USERNAME, message_text= """Hello, I'm your personal companion. 😄\n
+                                                                            You can talk with me and ask me an intresting question.""")
 
 
-
+# define a function to react on text messages.
 async def text_message_parser(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    """Saves a message to a JSON file."""
+    """Saves a message to a JSON file and generates a response using the OpenAI API 
+    and google.cloud text_to_speech.
+
+    Args:
+        update (Update): The update object.
+        context (ContextTypes.DEFAULT_TYPE): The context object.
+    """
+
+    # Writed "if" to not get "update" from edited messages in future could used to process updated message.
     if update.edited_message:
         pass
     else:
         start_time = time.time()
 
+        # Get the user information.
         chat_id = update.effective_chat.id
         user_id = update.effective_user.id
         message_id = update.message.message_id
@@ -218,28 +320,39 @@ async def text_message_parser(update: Update, context: ContextTypes.DEFAULT_TYPE
 
         await context.bot.send_message(chat_id, ".")
 
+        # Save the message to the chat history.
         save_message(chat_name, chat_id, user_id, message_id, username, message_text)
         start_load_save = time.time()
 
+        # Load the chat history and make chat bot promt to completion.    
         chat_history = load_json_file(chat_name)
         chatbot_messages = make_chatbot_history(chat_history)
+        
         print(f'\t\t\tLOAD SAVE TAKE - {time.time() - start_load_save}S')
 
         start_compl = time.time()
+
+        # Generate a response using the OpenAI API.
         result = get_completion(chatbot_messages)
+
         print(f'\t\t\tCOMPL TAKE - {time.time() - start_compl}S')
 
         await context.bot.edit_message_text(". .", chat_id, message_id+1)
 
         start_lang = time.time()
+
+        # Try check languge of copmletion text message
         try:
             languge_code = detect(result)
         except langdetect.lang_detect_exception.LangDetectException:
             print("Can't detect language so using 'en'")
             languge_code = 'en'
+
         print(f'\t\t\tLANG DETECT TAKE - {time.time() - start_lang}S')
 
         start_speech = time.time()
+
+        # Generate voice message from gpt text completion using checked language before.
         try:
             synthesis_input = texttospeech.SynthesisInput(text=result)
             voice = texttospeech.VoiceSelectionParams(language_code=languge_code, ssml_gender=texttospeech.SsmlVoiceGender.FEMALE )
@@ -254,20 +367,30 @@ async def text_message_parser(update: Update, context: ContextTypes.DEFAULT_TYPE
 
         await context.bot.edit_message_text(". . .", chat_id, message_id+1)
 
+        # Save gpt answer to chat history.
         save_message(chat_name, chat_id, user_id, message_id+2, BOT_USERNAME, result)
 
         start_send = time.time()
 
+        # Send the response to the user.
         await context.bot.send_voice(chat_id, speech)
         await context.bot.send_message(chat_id, result)
+
         print(f'\t\t\tSEND TAKE - {time.time() - start_send}S')
+
         print(f'\t\t\tREQUEST TAKE - {time.time() - start_time}S')
                             
 if __name__ == '__main__':
+    # Check if the needed paths exist.
     check_if_needed_path_exist()
+
+    # Load the API keys.
     keys_dict = load_json_file('keys', PATH_KEYS_ACCESS)
+
+    # Write gpt model name to token counter.
     token_encoder = tiktoken.encoding_for_model(AI_MODEL_NAME)
 
+    # Try initialize the OpenAI API and the Google Cloud Text-to-Speech API.
     try:
         openai.api_key = keys_dict['openai']
         application = ApplicationBuilder().token(keys_dict['telegram']).build()
@@ -276,10 +399,13 @@ if __name__ == '__main__':
         print(f'There is no API keys in folder "{PATH_KEYS_ACCESS}" or they are not correct.')
         sys.exit(1)
     
+    # Create the handlers.
     start_handler = CommandHandler('start', start)
     message_handler = MessageHandler(filters.TEXT, text_message_parser)
 
+    # Add the handlers to the application.
     application.add_handler(start_handler)
     application.add_handler(message_handler)
     
+    # Run the application.
     application.run_polling()
